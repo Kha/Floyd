e48bb119ae7ecbd43d0b2bd9f10a22031de5ad79
Sebastian Ullrich <sebastian@honoghr.de>
2011-05-07 15:32:10 +0200

    Implement dithering algorithm

--- src/edu/kit/ipd/fsdither/Program.java
+++ src/edu/kit/ipd/fsdither/Program.java
@@ -9,7 +9,10 @@ import javax.imageio.ImageIO;
  * Main class of the program.
  * 
  */
-public class Program {
+public final class Program {
+	private Program() {
+
+	}
 
 	/**
 	 * Entry point of the program.
@@ -30,5 +33,60 @@ public class Program {
 			System.out.println("Input file couldn't be read: " + e.getMessage());
 			return;
 		}
+
+		floydSteinbergDither(input);
+
+		try {
+			ImageIO.write(input, "PNG", config.getTarget());
+		} catch (IOException e) {
+			System.out.println("Output file couldn't be written: " + e.getMessage());
+			return;
+		}
+	}
+
+	private static final double[] DISTRIBUTION = new double[] { 0, 0, 0, 0, 0, 7. / 16, 3. / 16, 5. / 16, 1. / 16 };
+
+	// See http://en.wikipedia.org/wiki/Floyd%E2%80%93Steinberg_dithering
+	private static void floydSteinbergDither(BufferedImage image) {
+		for (int y = 0; y < image.getHeight(); y++) {
+			for (int x = 0; x < image.getWidth(); x++) {
+				int[] rgb = colorToRGB(image.getRGB(x, y));
+				int[] newPixel = new int[3];
+
+				for (int channel = 0; channel < 3; channel++) {
+					newPixel[channel] = (rgb[channel] + 128) / 256 * 255;
+				}
+				image.setRGB(x, y, rgbToColor(newPixel));
+
+				for (int dy = -1; dy <= 1; dy++) {
+					for (int dx = -1; dx <= 1; dx++) {
+						int y2 = y + dy;
+						int x2 = x + dx;
+						if (y2 >= 0 && y2 < image.getHeight() && x2 >= 0 && x2 < image.getWidth()) {
+							int[] rgb2 = colorToRGB(image.getRGB(x2, y2));
+							for (int channel = 0; channel < 3; channel++) {
+								rgb2[channel] =
+										clampToByte(rgb2[channel]
+												+ (int) ((rgb[channel] - newPixel[channel]) * DISTRIBUTION[(dy + 1) * 3
+														+ dx + 1]));
+							}
+							image.setRGB(x2, y2, rgbToColor(rgb2));
+						}
+					}
+				}
+			}
+		}
+	}
+
+	private static int[] colorToRGB(int color) {
+		return new int[] { (color >>> 16) & 0xFF, (color >>> 8) & 0xFF, color & 0xFF };
+	}
+
+	private static int rgbToColor(int[] rgb) {
+		return rgb[0] << 16 | rgb[1] << 8 | rgb[2];
+	}
+
+	private static int clampToByte(int value) {
+		return Math.max(0, Math.min(255, value));
 	}
 }
