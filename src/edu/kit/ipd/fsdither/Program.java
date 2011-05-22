d2c69ddc39d9fd7b6123a3c6c1eb73d329601f9a
Sebastian Ullrich <sebasti@nullrich.de>
2011-05-22 19:42:31 +0200

    Show source picture

--- src/edu/kit/ipd/fsdither/Program.java
+++ src/edu/kit/ipd/fsdither/Program.java
@@ -1,14 +1,130 @@
 package edu.kit.ipd.fsdither;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 /**
  * Main class of the program.
  * 
  */
-public final class Program {
-	private Program() {
+public final class Program extends JFrame {
+	private static final long serialVersionUID = 1L;
+	// The 3x3 distribution matrix of the algorithm
+	private static final double[][] DISTRIBUTION = { { 0, 0, 0 },
+			{ 0, 0, 7. / 16 }, { 3. / 16, 5. / 16, 1. / 16 } };
 
+	private BufferedImage source;
+	private final BufferedImage reviewImage = new BufferedImage(150, 150,
+			BufferedImage.TYPE_INT_RGB);
+	private final BufferedImage previewImage = new BufferedImage(150, 150,
+			BufferedImage.TYPE_INT_RGB);
+	private File target;
+
+	private final JButton floydButton;
+
+	private Program() {
+		super("Floyd-Steinberg");
+
+		JPanel centerPanel = createCenterPanel();
+
+		JPanel bottomPanel = new JPanel(new BorderLayout());
+
+		floydButton = new JButton("Bild auf 3 Bit reduzieren");
+		floydButton.setEnabled(false);
+
+		JSlider spaceSlider = new JSlider(0, 6, 0);
+
+		bottomPanel.add(spaceSlider, BorderLayout.LINE_START);
+		bottomPanel.add(floydButton);
+
+		add(centerPanel);
+		add(bottomPanel, BorderLayout.PAGE_END);
+
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		pack();
+	}
+
+	private JPanel createCenterPanel() {
+		JPanel centerPanel = new JPanel(new GridLayout(2, 2));
+
+		final JButton sourceButton = new JButton("Quelldatei wählen");
+		sourceButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser chooser = new JFileChooser();
+				chooser.setFileFilter(new FileNameExtensionFilter("PNG", "png"));
+				if (chooser.showOpenDialog(Program.this) == JFileChooser.APPROVE_OPTION) {
+					try {
+						source = ImageIO.read(chooser.getSelectedFile());
+					} catch (IOException e1) {
+						JOptionPane.showMessageDialog(Program.this,
+								"Quelldatei konnte nicht gelesen werden",
+								"Fehler", JOptionPane.ERROR_MESSAGE);
+					}
+					sourceButton.setText("Quelldatei: " + source);
+
+					if (source != null && target != null) {
+						floydButton.setEnabled(true);
+					}
+					updateViews();
+				}
+			}
+		});
+
+		JButton targetButton = new JButton("Zieldatei wählen");
+		targetButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser chooser = new JFileChooser();
+				chooser.setFileFilter(new FileNameExtensionFilter("PNG", "png"));
+				if (chooser.showSaveDialog(Program.this) == JFileChooser.APPROVE_OPTION) {
+					target = chooser.getSelectedFile();
+					sourceButton.setText("Zieldatei: " + target);
+
+					if (source != null && target != null) {
+						floydButton.setEnabled(true);
+					}
+				}
+			}
+		});
+
+		ImageIcon reviewIcon = new ImageIcon(reviewImage);
+		ImageIcon previewIcon = new ImageIcon(previewImage);
+
+		centerPanel.add(sourceButton);
+		centerPanel.add(targetButton);
+		centerPanel.add(new JLabel(reviewIcon));
+		centerPanel.add(new JLabel(previewIcon));
+		return centerPanel;
+	}
+
+	private void updateViews() {
+		BufferedImage result = new BufferedImage(source.getWidth(),
+				source.getHeight(), source.getType());
+		result.setData(source.getData());
+		floydSteinbergDither(result);
+
+		reviewImage.setData(source.getData(new Rectangle(0, 0, 150, 150)));
+		previewImage.setData(result.getData());
 	}
 
 	/**
@@ -16,17 +132,21 @@ public final class Program {
 	 * 
 	 * @param args
 	 *            command line arguments - see Configuration.java
+	 * @throws UnsupportedLookAndFeelException
+	 *             because
+	 * @throws IllegalAccessException
+	 *             I don't
+	 * @throws InstantiationException
+	 *             wanna
+	 * @throws ClassNotFoundException
+	 *             catch those
 	 */
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws ClassNotFoundException,
+			InstantiationException, IllegalAccessException,
+			UnsupportedLookAndFeelException {
+		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		new Program().setVisible(true);
 	}
-
-	// The 3x3 distribution matrix of the algorithm
-	private static final double[][] DISTRIBUTION = {
-			{ 0, 0, 0 },
-			{ 0, 0, 7. / 16 },
-			{ 3. / 16, 5. / 16, 1. / 16 }
-	};
 
 	// See http://en.wikipedia.org/wiki/Floyd%E2%80%93Steinberg_dithering
 	private static void floydSteinbergDither(BufferedImage image) {
@@ -46,12 +166,12 @@ public final class Program {
 					for (int dx = -1; dx <= 1; dx++) {
 						int y2 = y + dy;
 						int x2 = x + dx;
-						if (y2 >= 0 && y2 < image.getHeight() && x2 >= 0 && x2 < image.getWidth()) {
+						if (y2 >= 0 && y2 < image.getHeight() && x2 >= 0
+								&& x2 < image.getWidth()) {
 							int[] rgb2 = colorToRGB(image.getRGB(x2, y2));
 							for (int channel = 0; channel < 3; channel++) {
 								rgb2[channel] = clampToByte(rgb2[channel]
-										+ (int) ((rgb[channel] - newPixel[channel]) * DISTRIBUTION[dy + 1][dx + 1])
-										);
+										+ (int) ((rgb[channel] - newPixel[channel]) * DISTRIBUTION[dy + 1][dx + 1]));
 							}
 							image.setRGB(x2, y2, rgbToColor(rgb2));
 						}
@@ -63,7 +183,8 @@ public final class Program {
 
 	// Extracts RGB channel values from color.
 	private static int[] colorToRGB(int color) {
-		return new int[] { (color >>> 16) & 0xFF, (color >>> 8) & 0xFF, color & 0xFF };
+		return new int[] { (color >>> 16) & 0xFF, (color >>> 8) & 0xFF,
+				color & 0xFF };
 	}
 
 	// Combines RGB channel values into color value.
