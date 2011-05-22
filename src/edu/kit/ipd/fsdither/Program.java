ef9b1ff1a2800f3ff8cfc82e491fe17a07c78248
Sebastian Ullrich <sebasti@nullrich.de>
2011-05-22 21:21:45 +0200

    Improve layout

--- src/edu/kit/ipd/fsdither/Program.java
+++ src/edu/kit/ipd/fsdither/Program.java
@@ -2,7 +2,6 @@ package edu.kit.ipd.fsdither;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
@@ -10,6 +9,8 @@ import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
@@ -18,6 +19,7 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
+import javax.swing.JTextField;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.event.ChangeEvent;
@@ -29,66 +31,116 @@ import javax.swing.filechooser.FileNameExtensionFilter;
  * 
  */
 public final class Program extends JFrame {
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = -7816979531719395250L;
+
 	// The 3x3 distribution matrix of the algorithm
-	private static final double[][] DISTRIBUTION = { { 0, 0, 0 },
-			{ 0, 0, 7. / 16 }, { 3. / 16, 5. / 16, 1. / 16 } };
+	private static final double[][] DISTRIBUTION = {
+			{ 0, 0, 0 },
+			{ 0, 0, 7. / 16 },
+			{ 3. / 16, 5. / 16, 1. / 16 }
+	};
+
+	private static final class FilePanel extends JPanel {
+		private static final long serialVersionUID = 3673273071068438357L;
+
+		JTextField pathField = new JTextField();
+		JButton chooseButton = new JButton("...");
+		JLabel imageLabel = new JLabel();
+
+		private FilePanel(String title) {
+			setBorder(BorderFactory.createTitledBorder(title));
+			setLayout(new BorderLayout());
+
+			pathField.setEditable(false);
+
+			JPanel pathPanel = new JPanel();
+			pathPanel.setLayout(new BoxLayout(pathPanel, BoxLayout.LINE_AXIS));
+			pathPanel.add(pathField);
+			pathPanel.add(chooseButton);
+
+			imageLabel.setPreferredSize(new Dimension(150, 150));
+
+			add(pathPanel, BorderLayout.PAGE_START);
+			add(imageLabel, BorderLayout.CENTER);
+		}
+	}
+
 
 	private BufferedImage source;
 	private File target;
 
 	private final JButton floydButton;
 	private JSlider bitSlider;
-	private JLabel reviewLabel;
-	private JLabel previewLabel;
+	private FilePanel sourcePanel;
+	private FilePanel targetPanel;
 
 	private Program() {
 		super("Floyd-Steinberg");
 
-		JPanel centerPanel = createCenterPanel();
+		setLayout(new BoxLayout(getContentPane(), BoxLayout.PAGE_AXIS));
 
-		JPanel bottomPanel = new JPanel(new BorderLayout());
+		JPanel imagesPanel = createImagesPanel();
 
-		floydButton = new JButton("Bild auf 3 Bit reduzieren");
-		floydButton.setEnabled(false);
 
-		bitSlider = new JSlider(1, 8, 8);
+		JPanel optionsPanel = new JPanel(new BorderLayout());
+		optionsPanel.setBorder(BorderFactory
+				.createTitledBorder("Farbtiefe wählen:"));
+
+		bitSlider = new JSlider(3, 24, 3);
+		bitSlider.setMajorTickSpacing(3);
+		bitSlider.setMinorTickSpacing(3);
+		bitSlider.setSnapToTicks(true);
+		bitSlider.setPaintTicks(true);
+		bitSlider.setPaintLabels(true);
+
 		bitSlider.addChangeListener(new ChangeListener() {
 			@Override
 			public void stateChanged(ChangeEvent e) {
-				updateViews();
+				if (source != null && target != null) {
+					updateViews();
+				}
 			}
 		});
 
-		bottomPanel.add(bitSlider, BorderLayout.LINE_START);
-		bottomPanel.add(floydButton);
+		optionsPanel.add(bitSlider);
 
-		add(centerPanel);
-		add(bottomPanel, BorderLayout.PAGE_END);
+
+		floydButton = new JButton("Starte Reduzierung");
+		floydButton.setEnabled(false);
+
+
+		add(imagesPanel);
+		add(optionsPanel);
+		add(floydButton);
 
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setResizable(false);
 		pack();
 	}
 
-	private JPanel createCenterPanel() {
-		JPanel centerPanel = new JPanel(new GridLayout(2, 2));
+	private JPanel createImagesPanel() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new BoxLayout(panel, BoxLayout.LINE_AXIS));
 
-		final JButton sourceButton = new JButton("Quelldatei wählen");
-		sourceButton.addActionListener(new ActionListener() {
+		sourcePanel = new FilePanel("Quelldatei:");
+
+		sourcePanel.chooseButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				JFileChooser chooser = new JFileChooser();
 				chooser.setFileFilter(new FileNameExtensionFilter("PNG", "png"));
 				if (chooser.showOpenDialog(Program.this) == JFileChooser.APPROVE_OPTION) {
+					File file = chooser.getSelectedFile();
 					try {
-						source = ImageIO.read(chooser.getSelectedFile());
+						source = ImageIO.read(file);
 					} catch (IOException e1) {
 						JOptionPane.showMessageDialog(Program.this,
 								"Quelldatei konnte nicht gelesen werden",
 								"Fehler", JOptionPane.ERROR_MESSAGE);
+						return;
 					}
-					sourceButton.setText("Quelldatei: " + source);
 
+					sourcePanel.pathField.setText(file.getPath());
 					if (source != null && target != null) {
 						floydButton.setEnabled(true);
 					}
@@ -97,16 +149,17 @@ public final class Program extends JFrame {
 			}
 		});
 
-		JButton targetButton = new JButton("Zieldatei wählen");
-		targetButton.addActionListener(new ActionListener() {
+		targetPanel = new FilePanel("Zieldatei:");
+
+		targetPanel.chooseButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				JFileChooser chooser = new JFileChooser();
 				chooser.setFileFilter(new FileNameExtensionFilter("PNG", "png"));
 				if (chooser.showSaveDialog(Program.this) == JFileChooser.APPROVE_OPTION) {
 					target = chooser.getSelectedFile();
-					sourceButton.setText("Zieldatei: " + target);
 
+					targetPanel.pathField.setText(target.getPath());
 					if (source != null && target != null) {
 						floydButton.setEnabled(true);
 					}
@@ -114,25 +167,17 @@ public final class Program extends JFrame {
 			}
 		});
 
-		reviewLabel = new JLabel();
-		reviewLabel.setPreferredSize(new Dimension(150, 150));
-
-		previewLabel = new JLabel();
-		previewLabel.setPreferredSize(new Dimension(150, 150));
-
-		centerPanel.add(sourceButton);
-		centerPanel.add(targetButton);
-		centerPanel.add(reviewLabel);
-		centerPanel.add(previewLabel);
-		return centerPanel;
+		panel.add(sourcePanel);
+		panel.add(targetPanel);
+		return panel;
 	}
 
 	private void updateViews() {
 		BufferedImage result = cloneImage(source);
-		floydSteinbergDither(result, bitSlider.getValue());
+		floydSteinbergDither(result, bitSlider.getValue() / 3);
 
-		reviewLabel.setIcon(new ImageIcon(cloneImage(source, 150, 150)));
-		previewLabel.setIcon(new ImageIcon(cloneImage(result, 150, 150)));
+		sourcePanel.imageLabel.setIcon(new ImageIcon(cloneImage(source, 150, 150)));
+		targetPanel.imageLabel.setIcon(new ImageIcon(cloneImage(result, 150, 150)));
 	}
 
 	private static BufferedImage cloneImage(BufferedImage image, int width,
